9a025b1880ba74006b8340c2236f81ec5c9f600f
Brian Smith <briaguya@gmail.com>
2019-06-27 08:10:14 -0500

    added JanisBasePage without breaking the build

--- joplin/home/models.py
+++ joplin/home/models.py
@@ -1,7 +1,32 @@
 from django.db import models
 
 from wagtail.core.models import Page
+from wagtail.search import index
+from wagtail.core.fields import RichTextField
 
 
 class HomePage(Page):
     pass
+
+class JanisBasePage(Page):
+    parent_page_types = ['base.HomePage']
+    subpage_types = []
+    search_fields = Page.search_fields + [
+        index.RelatedFields('owner', [
+            index.SearchField('last_name', partial_match=True),
+            index.FilterField('last_name'),
+        ])
+    ]
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    author_notes = RichTextField(
+        # max_length=DEFAULT_MAX_LENGTH,
+        features=['ul', 'ol', 'link'],
+        blank=True,
+        verbose_name='Notes for authors (Not visible on the resident facing site)'
+    )
+
+    class Meta:
+        abstract = True
